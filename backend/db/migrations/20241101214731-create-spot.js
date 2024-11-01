b52b3811a339b908b0912d601e392ae548737791
Sqhy4445 <nchen093@gmail.com>
2024-11-01 19:43:28 -0400

    booking and spot and spotImage issue

--- backend/db/migrations/20241101214731-create-spot.js
+++ backend/db/migrations/20241101214731-create-spot.js
@@ -14,7 +14,8 @@ module.exports = {
         allowNull: false,
         references: {
           model: "Users",
-          key: 'id'
+          key: 'id',
+          as: 'OwnerId'
         },
         onDelete: "CASCADE",
       },
